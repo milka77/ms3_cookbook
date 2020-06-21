ae85d583f7bec50a2296dcbd033d06d270b8966b
milka77 <55999046+milka77@users.noreply.github.com>
2020-06-21 11:14:41 +0100

    Added a JavaScript function to change the background color while scrolling

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
+/* Adding new row function at the newrecipe form to the textarea fields */ 
 function adjustHeight(el){
     el.style.height = (el.scrollHeight > el.clientHeight) ? (el.scrollHeight)+"px" : "41px";
 }
+
+/* Changing the background color for the navigation bar */
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 225) {
+            $('#nav').removeClass('cook-navbar');
+            $('#nav').addClass('bg-light');
+            $('#nav').addClass('navbar-light');
+        } else {
+            $('#nav').removeClass('bg-light');
+            $('#nav').removeClass('navbar-light');
+            $('#nav').addClass('cook-navbar');
+        }
+    })
+}) 
