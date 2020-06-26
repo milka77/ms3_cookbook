6c5d64aa91b9dd435e3c7a9a4360461bef080767
milka77 <55999046+milka77@users.noreply.github.com>
2020-06-26 19:36:15 +0100

    removed env.py from .gitignore list

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["MONGO_URI"] = 'mongodb+srv://milka:AsDeWq1210@mycluster-b8liz.mongodb.net/cook_book?retryWrites=true&w=majority'
+os.environ["SECRET_KEY"] = 'milkaAkaKrisztianKeseruAsddsa321'
