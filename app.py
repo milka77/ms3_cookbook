f845a3b23190a8796d86729705fcbd537b67a7ca
milka77 <55999046+milka77@users.noreply.github.com>
2020-06-28 19:32:56 +0100

    Beautify the code in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask import Flask, render_template, redirect, request, url_for
 import env as config
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
