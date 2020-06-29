a384f5f715f86472eba81403d10712a0b2bf0403
milka77 <55999046+milka77@users.noreply.github.com>
2020-06-29 11:53:57 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,4 +1,1 @@
 import os
-
-os.environ["MONGO_URI"] = 'mongodb+srv://milka:AsDeWq1210@mycluster-b8liz.mongodb.net/cook_book?retryWrites=true&w=majority'
-os.environ["SECRET_KEY"] = 'milkaAkaKrisztianKeseruAsddsa321'
