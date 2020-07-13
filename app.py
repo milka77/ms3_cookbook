6252c8dcb07936ae51f42a07a5076f1c444cfcf5
milka77 <55999046+milka77@users.noreply.github.com>
2020-07-13 09:30:58 +0100

    Update comments in app.py

--- app.py
+++ app.py
@@ -9,24 +9,32 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
-# Database (mongoDB) confugirations:
+"""
+Database (mongoDB) confugirations:
+"""
 app.config["MONGO_DBNAME"] = 'cook_book'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
 
-# Pagination mongoDB query
+
 def get_recipes(offset=0, per_page=10):
+    """
+    Pagination mongoDB query
+    """
     recipes = mongo.db.recipes.find()
     return recipes[offset: offset + per_page]
 
 
-# Routes and viwes
+
 @app.route('/')
-# index - View of the landing page with 4 rancom recipes from the database
 @app.route('/index')
 def index():
+    """
+    Routes and views
+    index - View of the landing page with 4 rancom recipes from the database
+    """
     recipe_numbers = mongo.db.recipes.count()
     _samples = mongo.db.recipes.aggregate([{'$sample': {'size': 4}}])
     return render_template(
@@ -36,9 +44,11 @@ def index():
                             )
 
 
-# View from the newrecipe.html to add new recipes to the database
 @app.route('/new_recipe')
 def new_recipe():
+    """
+    View from the newrecipe.html to add new recipes to the database
+    """
     return render_template(
                             'newrecipe.html',
                             categories=mongo.db.categories.find().sort(
@@ -47,15 +57,19 @@ def new_recipe():
                             )
 
 
-# Receiving the picture for the recipe from the database
 @app.route('/file/<filename>')
 def file(filename):
+    """
+    Receiving the picture for the recipe from the database
+    """
     return mongo.send_file(filename)
 
 
-# Insert_recipe function to add the recipe into the database
 @app.route('/insert_recipe', methods=["GET", "POST"])
 def insert_recipe():
+    """
+    Insert_recipe function to add the recipe into the database
+    """
     recipes = mongo.db.recipes
     ingredients = request.form.get('ingredients').splitlines()
     instructions = request.form.get('recipe_instructions').splitlines()
@@ -79,9 +93,11 @@ def insert_recipe():
     return redirect(url_for('recipes'))
 
 
-# Show all existing recipes from the database
 @app.route('/recipes')
 def recipes():
+    """
+    Show all existing recipes from the database
+    """
     page, per_page, offset = get_page_args(
                                             page_parameter='page',
                                             per_page_parameter='per_page'
@@ -100,23 +116,29 @@ def recipes():
                             )
 
 
-# Detailed page form the selected recipe
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
+    """
+    Detailed page form the selected recipe
+    """
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template('showrecipe.html', recipe=_recipe)
 
 
-# Deleting a recipe
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
+    """
+    Deleting a recipe
+    """
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     return redirect(url_for('recipes'))
 
 
-# Editting recipe page
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
+    """
+    Editting recipe page
+    """
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     _categories = mongo.db.categories.find().sort(
                                                     'category_name',
@@ -132,9 +154,11 @@ def edit_recipe(recipe_id):
                             )
 
 
-# Updating recipe function
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
+    """
+    Updating recipe function
+    """
     recipes = mongo.db.recipes
     ingredients = request.form.get('ingredients').splitlines()
     instructions = request.form.get('recipe_instructions').splitlines()
@@ -161,29 +185,38 @@ def update_recipe(recipe_id):
     return redirect(url_for('recipes'))
 
 
-# Navbar search function
 @app.route('/navbar_search', methods=["GET", "POST"])
 def navbar_search():
+    """
+    Navbar search function
+    """
     search_input = request.args.get('navbar_search')
     recipe_found = list(mongo.db.recipes.find(
         {"$text": {"$search": search_input}}))
     return render_template('result.html', results=recipe_found)
 
 
-# Contact us page
 @app.route('/contact_us', methods=["GET", "POST"])
 def contact_us():
+    """
+    Contact us page with a form and emailJS script
+    """
     return render_template('contactus.html')
 
 
 @app.route('/our_brand')
 def our_brand():
+    """
+    Our brand information page
+    """
     return render_template('ourbrand.html')
 
 
-# File (picture) upload function to MongoDB
 @app.route('/add_file', methods=["GET", "POST"])
 def add_file():
+    """
+    File (picture) upload function to MongoDB
+    """
     recipe_image = request.files['recipe_image']
     mongo.save_file(recipe_image.filename, recipe_image)
     mongo.db.recipes.insert({
